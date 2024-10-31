9fd8f1d50dbe1eea9d975ba6391c0ba1dce5e295
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-10-31 15:28:32 -0400

    Add mouseover and mouseout events to change cell color

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const container = document.querySelector("#container")
 
-
-for(let i = 0; i < 256; i++) {
-    const cell = document.createElement("div")
-    // cell.textContent = "Cell"
-    cell.classList.toggle("cell")
-    container.appendChild(cell)
+function changeColor (e) {
+    let color = e.target.style.backgroundColor
+    if (e.type === "mouseover") {
+        e.target.style.backgroundColor = "blue"
+    } else {
+        e.target.style.backgroundColor = "brown"   
+    }
 }
+
+let squresNumber = 16
+squresNumber *= squresNumber
+
+
+
+for(let i = 0; i < squresNumber; i++) {
+    const newCell = document.createElement("div")
+    newCell.classList.toggle("cell")
+    container.appendChild(newCell)
+}
+
+const cells = document.querySelectorAll(".cell")
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", changeColor )
+})
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseout", changeColor )
+})
