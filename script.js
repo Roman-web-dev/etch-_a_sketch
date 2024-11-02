7a86e052c42efd1e7303c0b0d5284851f1f83e6a
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-11-02 13:52:32 -0400

    Randomize color of cells, opacity decrease after interaction

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ const numInput = document.querySelector("#numInput")
 function changeColor (e) {
     let color = e.target.style.backgroundColor
     if (e.type === "mouseover") {
+        e.target.dataset.Color = color(value)
         e.target.style.backgroundColor = "blue"
     } else {
-        e.target.style.backgroundColor = "brown"   
+        e.target.style.backgroundColor = e.target.dataset.Color
+        let opacity = parseFloat(getComputedStyle(e.target).opacity)
+        opacity -= 0.2
+        e.target.style.opacity = opacity
     }
 }
 
+function randomRGB() {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(255);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
 function createGrid () {
     container.innerHTML = "";
 
@@ -25,6 +34,7 @@ function createGrid () {
         newCell.addEventListener("mouseover", changeColor )
         newCell.addEventListener("mouseout", changeColor )
         newCell.style.flex = `1 1 ${cellWidth}%`
+        newCell.style.backgroundColor = `rgb(${randomRGB()} ${randomRGB()} ${randomRGB()})`
     }
     const cells = document.querySelectorAll(".cell")    
 }
@@ -36,7 +46,7 @@ let squaresNumber = 16
 button.addEventListener("click", () => {
     squaresNumber = parseInt(numInput.value)
     if (squaresNumber > 100) {
-        alert("Must be less than 100")
+        alert("Max size 100")
     }else createGrid()
 })
 
